2e08562e8066571844b6780d29aa458857866027
Johannes Weidacher <jweidacher@gmail.com>
2020-05-30 22:30:56 +0200

    Updated to work without network mode host

--- xaseco/settings.sh
+++ xaseco/settings.sh
@@ -5,6 +5,7 @@ sed -i "s/<mysql_password>YOUR_MYSQL_PASSWORD/<mysql_password>${MYSQL_PASSWORD}/
 sed -i "s/<mysql_server>localhost/<mysql_server>${MYSQL_SERVER}/" localdatabase.xml
 sed -i "s/<\/masteradmins>/<tmlogin>${ASECO_MASTER_ADMIN}<\/tmlogin><\/masteradmins>/" config.xml
 sed -i "s/<password>YOUR_SUPERADMIN_PASSWORD/<password>${SERVER_SA_PASSWORD}/" config.xml
+sed -i "s/<ip>127.0.0.1/<ip>${SERVER_IP}/" config.xml
 sed -i "s/<\/admins>/<tmlogin>${ASECO_ADMIN}<\/tmlogin><\/admins>/" adminops.xml
 sed -i "s/\t<plugin>plugin.dedimania.php<\/plugin>/<\!--\t<plugin>plugin.dedimania.php<\/plugin> -->/" plugins.xml
 sed -i "s/\t<plugin>chat.dedimania.php<\/plugin>/<\!--\t<plugin>chat.dedimania.php<\/plugin> -->/" plugins.xml
